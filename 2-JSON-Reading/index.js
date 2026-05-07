93bdb0ffdc2376be6d40b32d70114d0deae42498
WayneHo78 <84441094+WayneHo78@users.noreply.github.com>
2026-05-07 07:07:49 +0000

    demo updated

--- 2-JSON-Reading/index.js
+++ 2-JSON-Reading/index.js
@@ -1,16 +1,21 @@
- async function loadData() {
-    const response = await axios.get("data.json");
-    console.log(response.data)
-    document.querySelector("#output")
-        .innerHTML = `
+const url = "https://raw.githubusercontent.com/kunxin-chor/data-files-and-stuff/master/json-simple.json";
 
-        <ul>
-                <li>Name: ${response.data.Name}</li>
-                <li>Mobile Number: ${response.data.Mobile}</li>
-                <li>Current Address: ${response.data["Permanent address"]}</li>
-                <li>Permanent Address ${response.data["current Address"]}</li>            
-            </ul>
-                   
-        `
+async function loadData() {
+    const response = await axios.get(url);
+    return response.data;
 }
-loadData();
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+    const userData = await loadData();
+
+    const outputUl = document.querySelector("#output");
+
+    outputUl.innerHTML = `
+        <li>Name: ${userData.Name}</li>
+        <li>Mobile Number: ${userData.Mobile}</li>
+        <li>Current Address: ${userData.Address["current Address"]}</li>
+        <li>Permanent Address: ${userData.Address["Permanent address"]}</li>
+    `;
+
+});
